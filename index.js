26531ee578c2147e9f0d5d5a0c022a23017fc448
franett <rfranett@gmail.com>
2024-10-10 19:36:28 -0300

    feat: adicionando  funcionalidade de busca na Wikipedia

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const readline = require('readline-sync')
+
 function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSearchTerm() {
-        return 'TERM EXAMPLE'
+        return readline.question('Type a Wikipedia search term: ')
+
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ['who is','what is','the history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
     }
 
     console.log(content)
